e8cbe4df3026a09c98ac4038fbc3339daa7797c6
nikenayu123 <ayuresdianan@gmail.com>
2026-04-05 13:31:19 +0700

    first commit

---
+++ projek_4/tugas_proses.java
@@ -0,0 +1,22 @@
+package projek_4;
+import java.util.Scanner;
+
+public class tugas_proses {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukan Nama:");
+        String nama=input.nextLine();
+        
+        String besar=nama.toUpperCase();
+        String kecil=nama.toLowerCase();
+
+        System.out.println("Huruf besar:"+besar);
+        System.out.println("Huruf kecil:"+ kecil);
+
+        input.close();
+        
+    }
+
+    
+}
---
+++ projek_p1/Hello_World.java
@@ -0,0 +1,8 @@
+package projek_p1;
+
+public class Hello_World {
+ public static void main(String[]args){
+    System.out.println("hello world");
+    System.out.println("hiiii aku Niken Ayu Resdianaaa");
+ }   
+}
